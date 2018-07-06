39203b9ac8cfd9a5bf080288d1a7624ae271d3fb
Edward Mwangi <muyaedward@gmail.com>
2018-07-06 04:57:47 +0300

    Fixed manage app.css issues

--- writerzbayform.php
+++ writerzbayform.php
@@ -7,7 +7,7 @@
 	Plugin Name: WriterzBay Order Form
 	Plugin URI: https://github.com/muyaedward/writerzbayform
 	Description: Wordpress plugin to manage orders in writerzbay website.
-	Version: 2.0.29
+	Version: 2.0.30
 	Author: Edward Muya Mwangi
 	Author URI: https://www.writersbayapp.com
 	Licence: GPLv2 or later
